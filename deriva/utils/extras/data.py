70a271626264d7da46ea3fc94ec081004ec6f6c2
Aref Shafaei <aref.shafaei@gmail.com>
2026-08-21 17:03:40 -0400

    Added support for skipping onconflict param in insert_if_not_exist
    This also fixes a bug in approx_json_bytecnt function

--- deriva/utils/extras/data.py
+++ deriva/utils/extras/data.py
@@ -31,7 +31,7 @@ def approx_json_bytecnt(data):
         bytecnt = 2
         for k, v in data.items():
             # key + ':' val + ','
-            bytecnt = 2 + approx_json_bytecnt(k) + approx_json_bytecnt(v)
+            bytecnt += 2 + approx_json_bytecnt(k) + approx_json_bytecnt(v)
         return bytecnt
     elif isinstance(data, str):
         # '"' + UTF8 string + '"'
@@ -84,14 +84,16 @@ def get_batch_len(payload, index=0, batch_size=10000, batch_bytes=2000000, decre
     return nrows 
 
 # ---------------------------------------------------------------
-def insert_if_not_exist(catalog, schema_name, table_name, payload, defaults=None, batch_size=10000, batch_bytes=2000000):
+def insert_if_not_exist(catalog, schema_name, table_name, payload, defaults=None, batch_size=10000, batch_bytes=2000000, onconflict="skip"):
     if not payload:
         return []
 
+    query_params = []
+    if onconflict:
+        query_params.append('onconflict=%s' % (urlquote(onconflict)))
     if defaults:
-        defaults_str = '&defaults=%s' % (','.join(list(map(urlquote, defaults))))
-    else:
-        defaults_str = ''
+        query_params.append('defaults=%s' % (','.join(list(map(urlquote, defaults)))))
+    query_str = '?%s' % ('&'.join(query_params)) if query_params else ''
 
     inserted = []
     index = 0
@@ -111,7 +113,7 @@ def insert_if_not_exist(catalog, schema_name, table_name, payload, defaults=None
         batch = payload[index:index+nrows]
         #print("index=%d nrows=%d bytes=%d" % (index, nrows, bytes))
         resp = catalog.post(
-            "/entity/%s:%s?onconflict=skip%s" % (urlquote(schema_name), urlquote(table_name), defaults_str),
+            "/entity/%s:%s%s" % (urlquote(schema_name), urlquote(table_name), query_str),
             json=batch
         )
         inserted.extend(resp.json())
